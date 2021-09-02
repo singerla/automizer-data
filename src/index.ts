c1e3d923295ae882a64f15238f85639235269aa5
singerla <tsinger@gmx.de>
2021-09-02 09:02:48 +0200

    chore(types): add types to export

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,12 @@ import { Query } from './query'
 import { Store } from './store'
 import { Parser } from './parser/parser'
 import { Gesstabs } from './parser/gesstabs'
-import { DataTag } from './types'
+import {DataGrid, DataGridCategories, DataPointFilter, DataTag } from './types'
 import { all, filterByDataTag, filterBy } from './filter';
 import { value, valueMeta, difference, points, dump } from './cell';
 import { byColId } from './sort';
 import { ParserOptions, RawResultInfo, StoreOptions, Tagger } from './types';
+import { getNestedClause } from './helper'
 
 const getData = async(selector: DataTag[] | DataTag[][], grid: any, prisma?: PrismaClient) => {
   if(!prisma) {
@@ -45,4 +46,6 @@ const sort = {
 }
 
 export type { ParserOptions, RawResultInfo, StoreOptions, Tagger }
-export { Parser, Gesstabs, Store, filter, cell, sort, getData, getDataObject }
+export type {DataGrid, DataGridCategories, DataPointFilter}
+export { Query, Parser, Gesstabs, Store, filter, cell, sort, getData, getDataObject }
+export { getNestedClause }
