207c1c1573e16fc28ef20f35c8c6b9ac3534fa54
singerla <tsinger@gmx.de>
2022-05-19 16:39:14 +0200

    fix(result): show proper body cell styles with active row labels

--- src/result.ts
+++ src/result.ts
@@ -6,8 +6,8 @@ import {
   Result as ResultType,
   ResultCell,
   ResultColumn,
-  ResultRow
-} from './types';
+  ResultRow,
+} from "./types";
 import {
   ChartBubble,
   ChartCategory,
@@ -17,321 +17,330 @@ import {
   TableData,
   TableRow,
   TableRowStyle,
-  TextStyle
-} from 'pptx-automizer';
-import Query from './query';
+  TextStyle,
+} from "pptx-automizer";
+import Query from "./query";
 
 export default class Result {
-  result: ResultType
-  inputKeys: CellKeys
-  keys: CellKeys
+  result: ResultType;
+  inputKeys: CellKeys;
+  keys: CellKeys;
   visibleKeys: {
-    row: string[],
-    column: string[]
-  }
-  allSheets: Datasheet[]
-  tags: any[]
-  metaParams: MetaParam
+    row: string[];
+    column: string[];
+  };
+  allSheets: Datasheet[];
+  tags: any[];
+  metaParams: MetaParam;
 
   constructor(query: Query) {
-    this.result = query.result
-    this.inputKeys = query.inputKeys
-    this.keys = query.keys
-    this.visibleKeys = query.visibleKeys
-    this.allSheets = query.allSheets
-    this.tags = <any>[]
-    this.metaParams = <MetaParam> {}
+    this.result = query.result;
+    this.inputKeys = query.inputKeys;
+    this.keys = query.keys;
+    this.visibleKeys = query.visibleKeys;
+    this.allSheets = query.allSheets;
+    this.tags = <any>[];
+    this.metaParams = <MetaParam>{};
   }
 
-  setMetaParams(params:any) {
-    this.metaParams = params
+  setMetaParams(params: any) {
+    this.metaParams = params;
   }
 
   getSeries = (): ChartSeries[] => {
-    return this.result.body[0].cols.map(col => {
+    return this.result.body[0].cols.map((col) => {
       return {
         label: col.key,
-        style: col.style
-      }
-    })
-  }
+        style: col.style,
+      };
+    });
+  };
 
   toSeriesCategories(): ChartData {
-    if(this.result.body[0]) {
-      const series = this.getSeries()
-      const categories = <ChartCategory[]> []
+    if (this.result.body[0]) {
+      const series = this.getSeries();
+      const categories = <ChartCategory[]>[];
 
-      this.result.body.forEach(row => {
+      this.result.body.forEach((row) => {
         categories.push({
           label: row.key,
-          values: row.cols.map(column => this.toNumber(column)),
-          styles: this.applyStyleCallback<ChartValueStyle>(row)
-        })
-      })
+          values: row.cols.map((column) => this.toNumber(column)),
+          styles: this.applyStyleCallback<ChartValueStyle>(row),
+        });
+      });
 
       return {
         series: series,
-        categories: categories
-      }
+        categories: categories,
+      };
     }
 
     return {
       series: [],
-      categories: []
-    }
+      categories: [],
+    };
   }
 
   toVerticalLines(): ChartData {
-    const series = this.getSeries()
-    const categories = <ChartCategory[]> []
+    const series = this.getSeries();
+    const categories = <ChartCategory[]>[];
 
     this.result.body.forEach((row, r) => {
       categories.push({
         label: row.key,
         y: r,
-        values: row.cols.map(cell => this.toNumber(cell)),
-      })
-    })
+        values: row.cols.map((cell) => this.toNumber(cell)),
+      });
+    });
 
     return {
       series: series,
-      categories: categories
-    }
+      categories: categories,
+    };
   }
 
   toCombo(): ChartData {
-    const series = this.getSeries()
-    const categories = <ChartCategory[]> []
+    const series = this.getSeries();
+    const categories = <ChartCategory[]>[];
 
     this.result.body.forEach((row, r) => {
       categories.push({
         label: row.key,
         y: r + 0.5,
-        values: row.cols.map(cell => this.toNumber(cell)),
-        styles: this.parseValueStyle(row.cols)
-      })
-    })
+        values: row.cols.map((cell) => this.toNumber(cell)),
+        styles: this.parseValueStyle(row.cols),
+      });
+    });
 
     return {
       series: series,
-      categories: categories
-    }
+      categories: categories,
+    };
   }
 
   toScatter(): ChartData {
-    const series = this.getSeries()
-    const categories = <ChartCategory[]> []
+    const series = this.getSeries();
+    const categories = <ChartCategory[]>[];
 
-    this.result.body.forEach((row:any, r) => {
+    this.result.body.forEach((row: any, r) => {
       categories.push({
         label: row.key,
-        values: row.cols.map((col:any) => {
+        values: row.cols.map((col: any) => {
           return {
             x: Number(col.value[0].value),
-            y: Number(col.value[1].value)
-          }
+            y: Number(col.value[1].value),
+          };
         }),
-        styles: this.applyStyleCallback<ChartValueStyle>(row)
-      })
-    })
+        styles: this.applyStyleCallback<ChartValueStyle>(row),
+      });
+    });
 
     return {
       series: series,
-      categories: categories
-    }
+      categories: categories,
+    };
   }
 
   toBubbles(): ChartData {
-    const series = this.getSeries()
-    const categories = <ChartCategory[]> []
+    const series = this.getSeries();
+    const categories = <ChartCategory[]>[];
 
-    this.result.body.forEach((row:any, r) => {
+    this.result.body.forEach((row: any, r) => {
       categories.push({
         label: row.key,
-        values: row.cols.map((col:any) => {
-          return <ChartBubble> {
+        values: row.cols.map((col: any) => {
+          return <ChartBubble>{
             size: Number(col.value[0].value),
             x: Number(col.value[1].value),
             y: Number(col.value[2].value),
-          }
+          };
         }),
-        styles: this.applyStyleCallback<ChartValueStyle>(row)
-      })
-    })
+        styles: this.applyStyleCallback<ChartValueStyle>(row),
+      });
+    });
 
     return {
       series: series,
-      categories: categories
-    }
+      categories: categories,
+    };
   }
 
   toLabels(): TableData {
     return {
-      body: this.result.body.map(row => {
+      body: this.result.body.map((row) => {
         return {
-          values: [ row.key ],
-          styles: this.applyStyleCallback<TableRowStyle>(row)
-        }
-      })
-    }
+          values: [row.key],
+          styles: this.applyStyleCallback<TableRowStyle>(row),
+        };
+      }),
+    };
   }
 
   toRowLabels(): TableData {
-    return this.toLabels()
+    return this.toLabels();
   }
 
   toColumnLabels(): TableData {
-    let series = <string[]>[]
-    let styles = <any>[]
-    if(this.result.body && this.result.body.length) {
-      series = this.result.body[0].cols.map(col => col.key)
-      styles = this.applyStyleCallback<TableRowStyle>(this.result.body[0])
+    let series = <string[]>[];
+    let styles = <any>[];
+    if (this.result.body && this.result.body.length) {
+      series = this.result.body[0].cols.map((col) => col.key);
+      styles = this.applyStyleCallback<TableRowStyle>(this.result.body[0]);
     }
 
     return {
       body: [
         {
           values: series,
-          styles: styles
-        }
-      ]
-    }
+          styles: styles,
+        },
+      ],
+    };
   }
 
   toTableBody(): TableData {
     return {
-      body: this.result.body.map(row => {
+      body: this.result.body.map((row) => {
         return {
-          values: row.cols.map(cell => this.toNumber(cell))
-        }
+          values: row.cols.map((cell) => this.toNumber(cell)),
+        };
       }),
-    }
+    };
   }
 
-  toTable(params?:any): TableData {
-    const body = <TableRow[]>[]
+  toTable(params?: any): TableData {
+    const body = <TableRow[]>[];
 
-    if(!this.result.body[0]) {
+    if (!this.result.body[0]) {
       return {
         body: [
           {
-            values: []
-          }
-        ]
-      }
+            values: [],
+          },
+        ],
+      };
     }
 
-    if(params?.showColumnLabels) {
-      const header = []
-      if(params?.showRowLabels) {
-        header.push('')
+    if (params?.showColumnLabels) {
+      const header = [];
+      if (params?.showRowLabels) {
+        header.push("");
       }
-      header.push(...this.result.body[0].cols.map(col => col.key))
+      header.push(...this.result.body[0].cols.map((col) => col.key));
 
       body.push({
-        values: header
-      })
+        values: header,
+        // TODO: insert column label styles
+      });
     }
 
-    this.result.body.forEach((row,r) => {
-      const tableRow = []
-      if(params?.showRowLabels) {
-        tableRow.push(String(row.key))
+    this.result.body.forEach((row, r) => {
+      const tableRow = [];
+      if (params?.showRowLabels) {
+        tableRow.push(String(row.key));
       }
-      row.cols.forEach((cell,c) => {
-        tableRow.push(this.renderCellValue(cell))
-      })
+      row.cols.forEach((cell, c) => {
+        tableRow.push(this.renderCellValue(cell));
+      });
+
+      let rowStyles = this.applyStyleCallback<TableRowStyle>(
+        row,
+        params?.showRowLabels
+      );
 
       body.push({
         values: tableRow,
-        styles: this.applyStyleCallback<TableRowStyle>(row)
-      })
-    })
+        styles: rowStyles,
+      });
+    });
 
     return {
-      body: body
-    }
+      body: body,
+    };
   }
 
   toNumber(column: ResultColumn): any {
-    const value = this.renderCellValue(column)
-    if(value === '') return value
-    return Number(value)
+    const value = this.renderCellValue(column);
+    if (value === "") return value;
+    return Number(value);
   }
 
   renderCellValue(column: ResultColumn): ResultCell {
-    if(!column) return ''
+    if (!column) return "";
 
-    if(Array.isArray(column.value)) {
-      if(!column.value[0] || column.value[0].value === null) {
-        return ''
+    if (Array.isArray(column.value)) {
+      if (!column.value[0] || column.value[0].value === null) {
+        return "";
       }
 
-      if(column.value.length > 0) {
-        return column.value[0].value
+      if (column.value.length > 0) {
+        return column.value[0].value;
       }
     }
 
-    if(typeof column.value === 'number'
-      || typeof column.value === 'string') {
-      return column.value
+    if (typeof column.value === "number" || typeof column.value === "string") {
+      return column.value;
     }
 
-    return ''
+    return "";
   }
 
-  formatPointKeys(keys:any) {
-    const rowOrColumn = ['row', 'column', 'nested']
+  formatPointKeys(keys: any) {
+    const rowOrColumn = ["row", "column", "nested"];
 
-    const queryResultKeys = <QueryResultKeys> {
-      row: (keys.row) ? Object.keys(keys.row): [],
-      column: (keys.column) ? Object.keys(keys.column): [],
-      nested: (keys.nested) ? Object.keys(keys.nested): [],
-      category: []
-    }
+    const queryResultKeys = <QueryResultKeys>{
+      row: keys.row ? Object.keys(keys.row) : [],
+      column: keys.column ? Object.keys(keys.column) : [],
+      nested: keys.nested ? Object.keys(keys.nested) : [],
+      category: [],
+    };
 
-    for(const key in keys) {
-      if(!rowOrColumn.includes(key)) {
+    for (const key in keys) {
+      if (!rowOrColumn.includes(key)) {
         queryResultKeys.category.push({
           categoryId: Number(key),
-          keys: Object.keys(keys[key])
-        })
+          keys: Object.keys(keys[key]),
+        });
       }
     }
 
-    return queryResultKeys
+    return queryResultKeys;
   }
 
-  applyStyleCallback<T>(row: ResultRow): T[] | TextStyle[] | ChartValueStyle[] {
-    if(this.metaParams?.cb) {
-      return this.metaParams.cb(row, this.metaParams, this)
+  applyStyleCallback<T>(
+    row: ResultRow,
+    showRowLabels?: boolean
+  ): T[] | TextStyle[] | ChartValueStyle[] {
+    if (this.metaParams?.cb) {
+      return this.metaParams.cb(row, this.metaParams, this);
     }
 
-    const styles = <any[]>[]
+    const styles = <any[]>[];
+    if (showRowLabels) {
+      // TODO: insert row label style
+      styles.push(null);
+    }
     row.cols.forEach((column: ResultColumn) => {
-      if(column.value && Array.isArray(column.value)) {
-        column.value.forEach(point => {
-          if(point.style) {
-            styles.push(point.style)
+      if (column.value && Array.isArray(column.value)) {
+        column.value.forEach((point) => {
+          if (point.style) {
+            styles.push(point.style);
           } else {
-            styles.push(null)
+            styles.push(null);
           }
-        })
+        });
       }
-    })
+    });
 
-    return styles
+    return styles;
   }
 
   parseValueStyle(cols: ResultColumn[]) {
-    return cols.map(cell => {
-      if(Array.isArray(cell.value)
-        && cell.value[0]
-        && cell.value[0].style) {
-        return cell.value[0].style
+    return cols.map((cell) => {
+      if (Array.isArray(cell.value) && cell.value[0] && cell.value[0].style) {
+        return cell.value[0].style;
       }
-      return null
-    })
+      return null;
+    });
   }
-
 }
