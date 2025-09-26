37b9f1c2ec8d76d3d4b784fad5c1f6e8ab4e6c3d
singerla <tsinger@gmx.de>
2025-09-26 09:58:18 +0200

    fix(query): dev example ids for duckdb

--- src/dev.ts
+++ src/dev.ts
@@ -1,46 +1,39 @@
-import { vd } from "./helper";
-import { Store } from "./store";
-import { PrismaClient } from "./client";
-import {
-  Datasheet,
-  ParserOptions,
-  ParserType,
-  RawResultData,
-  RawResultMeta,
-  SplitDatasheetsRule,
-} from "./types/types";
-import { Pspp } from "./parser/pspp";
-import { Tagged } from "./parser/tagged";
 import { Query } from "./index";
 
 const run = async () => {
-  const selector = [
-    // [
-    //   {
-    //     category: "country",
-    //     value: "data/Originaler Datensatz aus dem System_Beispiel.sav",
-    //   },
-    //   {
-    //     category: "variable",
-    //     value: "B1a_19",
-    //   },
-    //   {
-    //     category: "subgroup",
-    //     value: "B1a_20",
-    //   },
-    // ],
-  ];
-
   const query = await Query.run({
     selector: [[1, 2, 4]],
     api: {
       driver: "DuckDB",
       endpoint: "http://localhost:5000/analyze",
-      mapCategoryIds: {
-        row: 1,
-        column: 2,
-        file: 3
-      }
+      variableCategoryId: 7,
+      splitCategoryId: 22,
+      mapCategoryIds: [
+        {
+          key: "country",
+          id: 1,
+        },
+        {
+          key: "wave",
+          id: 19,
+        },
+        {
+          key: "category",
+          id: 15,
+        },
+        {
+          key: "retailer",
+          id: 14,
+        },
+        {
+          key: "attribute",
+          id: 17,
+        },
+        {
+          key: "type",
+          id: 16,
+        },
+      ],
     },
   });
   const chartData = query.convert().toSeriesCategories();
